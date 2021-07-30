e8de08cca9666d58493e3bcea348aaa53bbd6de4
russellmyers <flipside@netspace.net.au>
2021-07-30 11:49:29 +1000

    v0.0.20c - Improve quiz responsiveness, other small responsive changes
    Cross Sim - Cross Type setting now has attr: form-control
    Home: Make copyright and nsf logo last
    Quz: Improve responsive

--- crosssim/forms.py
+++ crosssim/forms.py
@@ -6,9 +6,9 @@ class CrossSimForm(forms.Form):
     CROSS_TYPE_CHOICES = [('1', 'Pure-breeding line cross'), ('2', 'Test Cross'), ('3', 'Tri-Hybrid Cross'), ('4', 'Custom')]
     GEN_PHEN_CHOICES = [('g','Genotype'), ('p','Phenotype')]
     #p1 = forms.ChoiceField(widget=forms.RadioSelect(attrs = {'onchange' : "showChange(this);"}), choices=CHOICES)
-    alleles = forms.ChoiceField(widget=forms.RadioSelect, choices=ALLELE_CHOICES)
-    cross_type = forms.ChoiceField(widget=forms.Select, choices=CROSS_TYPE_CHOICES)
-    gen_phen = forms.ChoiceField(widget=forms.RadioSelect, choices=GEN_PHEN_CHOICES)
-    p1 = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class':'slim-button'}), choices=GEN_CHOICES)
+    alleles = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class': 'form-control'}), choices=ALLELE_CHOICES)
+    cross_type = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control mr-4'}), choices=CROSS_TYPE_CHOICES)
+    gen_phen = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class': 'form-control'}), choices=GEN_PHEN_CHOICES)
+    p1 = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class':'slim-button form-control'}), choices=GEN_CHOICES)
     p2 = forms.ChoiceField(widget=forms.RadioSelect, choices=GEN_CHOICES)
 
